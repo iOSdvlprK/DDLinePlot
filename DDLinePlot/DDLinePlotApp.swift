06a932a831d5f4ad89730affda636d49c5908535
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-02-06 21:20:28 +0900

    Initial Commit

---
+++ DDLinePlot/DDLinePlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDLinePlotApp.swift
+//  DDLinePlot
+//
+//  Created by joe on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DDLinePlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
