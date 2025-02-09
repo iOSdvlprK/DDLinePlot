036c8a7a9ac9ff0d734c6de75ced8a230fc51739
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-02-09 23:31:49 +0900

    implement cosine Taylor polynomial

--- DDLinePlot/CosineTaylorPolynomial.swift
+++ DDLinePlot/CosineTaylorPolynomial.swift
@@ -12,29 +12,32 @@ struct CosineTaylorPolynomial: View {
     var body: some View {
         Chart {
             LinePlot(x: "x", y: "y") { x in
-                cos(x)
+                taylorCosine(x: x, n: 0)
             }
             .foregroundStyle(.green)
-            
-            LinePlot(x: "x", y: "y") { x in
-                1
-            }
-            .foregroundStyle(.red)
-            
-            LinePlot(x: "x", y: "y") { x in
-                1 - 0.5 * x * x
-            }
-            .foregroundStyle(.blue)
-            
-            LinePlot(x: "x", y: "y") { x in
-                1 - 0.5 * x * x + (1/24)*x*x*x*x
-            }
-            .foregroundStyle(.blue)
         }
         .padding()
         .chartXScale(domain: -8...8)
         .chartYScale(domain: -8...8)
     }
+}
+
+func taylorCosine(x: Double, n: Double) -> Double {
+    var result = 0.0
+    var sign = 1.0
+    var xPower = 1.0
+    let xSquare = x * x
+    var evenFactorial = 1.0
+    
+    for k in 0...(Int(n+1)-1)/2 {
+        result += sign * xPower / evenFactorial
+        
+        sign *= -1
+        xPower *= xSquare
+        evenFactorial *= Double((2*k+1) * (2*k+2))
+    }
+    
+    return result
 }
 
 #Preview {
